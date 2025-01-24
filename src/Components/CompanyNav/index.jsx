f662c68ae80a8ed6bf97c18ff6636e05f0481b33
shahd <shahdmetwaly97@gmail.com>
2025-01-25 01:56:41 +0200

    translate section companyNav

--- src/Components/CompanyNav/index.jsx
+++ src/Components/CompanyNav/index.jsx
@@ -5,21 +5,22 @@ import { Grid, Paper } from '@mui/material';
 import { aboutIconCompany, aboutIconCompany2, aboutIconCompany3 } from '../../assets';
 import LiteYouTubeEmbed from 'react-lite-youtube-embed';
 import 'react-lite-youtube-embed/dist/LiteYouTubeEmbed.css';
+import { useTranslation } from 'react-i18next';
 
 const CompanyNav = ({ isOpen, setCompanyDrower }) => {
     const handleClose = () => {
         setCompanyDrower(false);
     };
-
+    const {t} = useTranslation()
     return (
         isOpen && (
             <Box
             sx={{
                 position: 'fixed',
                 top: 60,
-                right: 0.5,
+                right: {md:170 , xs:0.5},
                 width: {md:'850px' , xs:"100%"},
-                height: {md:'400px' , xs:"100%"},
+                height: {md:'auto' , xs:"100%"},
                 backgroundColor: '#FFF',
                 boxShadow: 3,
                 p: 3,
@@ -28,12 +29,12 @@ const CompanyNav = ({ isOpen, setCompanyDrower }) => {
                 overflowY:"auto"
             }}
         >
-                <Typography sx={{color:"black" , fontSize:"16px" ,
-                     px:15 , py:15}} onClick={handleClose}>Back to menu</Typography>
+                <Typography sx={{color:"black" , fontSize:"16px" , cursor:"pointer" , 
+                     px:{xs:15 }, py:{xs:15 , md:0.5}}} onClick={handleClose}>{t("Back to menu")}</Typography>
                 <Grid container>
                     <Grid item md={4} xs={12} sm={12} px={24} py={7} >
                         <Typography sx={{ color: 'black', fontSize: '16px', fontWeight: 500 }}>
-                            Company
+                            {t('Company')}
                         </Typography>
                         <Box display={'flex'} justifyContent={'center'} py={10}>
                             <Box display={'flex'} flexDirection={'row'}>
@@ -51,11 +52,10 @@ const CompanyNav = ({ isOpen, setCompanyDrower }) => {
                                 </Box>
                                 <Box display={'flex'} flexDirection={'column'} sx={{ marginInlineStart: '16px' }}>
                                     <Typography sx={{ color: 'black', fontWeight: 500 }}>
-                                        About
+                                       {t("About")} 
                                     </Typography>
                                     <Typography sx={{ color: 'rgb(30, 30, 30)', fontWeight: 300, fontSize: '14px' }}>
-                                        Streamline your first and last-mile deliveries,
-                                        boost efficiency, and improve customer satisfaction.
+                                      {t("Streamline your first and last-mile deliveries, boost efficiency, and improve customer satisfaction.")} 
                                     </Typography>
                                 </Box>
                             </Box>
@@ -77,10 +77,10 @@ const CompanyNav = ({ isOpen, setCompanyDrower }) => {
                                 </Box>
                                 <Box display={'flex'} flexDirection={'column'} sx={{ marginInlineStart: '16px' }}>
                                     <Typography sx={{ color: 'black', fontWeight: 500 }}>
-                                        Careers
+                                       {t("Careers")}
                                     </Typography>
                                     <Typography sx={{ color: 'rgb(30, 30, 30)', fontWeight: 300, fontSize: '14px' }}>
-                                        Join our team and grow your career with us.
+                                      {t("Join our team and grow your career with us.")} 
                                     </Typography>
                                 </Box>
                             </Box>
@@ -102,10 +102,10 @@ const CompanyNav = ({ isOpen, setCompanyDrower }) => {
                                 </Box>
                                 <Box display={'flex'} flexDirection={'column'} sx={{ marginInlineStart: '16px' }}>
                                     <Typography sx={{ color: 'black', fontWeight: 500 }}>
-                                        Contact Us
+                                      {t("Contact Us")}  
                                     </Typography>
                                     <Typography sx={{ color: 'rgb(30, 30, 30)', fontWeight: 300, fontSize: '14px' }}>
-                                        Discover how we can optimize your deliveries and help you scale.
+                                      {t("Discover how we can optimize your deliveries and help you scale.")} 
                                     </Typography>
                                 </Box>
                             </Box>
@@ -114,15 +114,15 @@ const CompanyNav = ({ isOpen, setCompanyDrower }) => {
 
                     <Grid item md={3} xs={12} sm={12} px={30}>
                         <Typography sx={{ color: 'black', fontSize: '16px', fontWeight: 500 }}>
-                            More
+                            {t("More")}
                         </Typography>
                         <Box py={15}>
                             <Typography sx={{ fontSize: '16px', color: 'black' }}>
-                                Privacy Policy
+                            {t('Privacy Policy')}
                             </Typography>
 
                             <Typography sx={{ fontSize: '16px', color: 'black', mt: 20 }}>
-                                Documentation
+                            {t('Documentation')}
                             </Typography>
                         </Box>
                     </Grid>
@@ -136,10 +136,10 @@ const CompanyNav = ({ isOpen, setCompanyDrower }) => {
                             />
                             <Box sx={{ flex: 1, p: '1.5rem' }}>
                                 <Typography sx={{ fontSize: '16px', fontWeight: 500, mt: 0 }}>
-                                    Enhancing operational efficiency and customer satisfaction
+                                   {t("Enhancing operational efficiency and customer satisfaction")} 
                                 </Typography>
                                 <Typography sx={{ fontSize: { xs: '18px', md: '14px' }, fontWeight: 300 }}>
-                                    Through innovative solutions, auto sorting, fleet management, route optimization, and real-time tracking.
+                                   {t('Through innovative solutions, auto sorting, fleet management, route optimization, and real-time tracking.')}
                                 </Typography>
                             </Box>
                         </Paper>
